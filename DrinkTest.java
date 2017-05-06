120cef95b7f288bd80d5645453934d3c993169bb
landeliu <landeliu@kth.se>
2017-05-06 16:47:20 +0200

    drink test

---
+++ DrinkTest.java
@@ -0,0 +1,62 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Cecilia Landelius on 2017-05-06.
+ */
+public class DrinkTest {
+
+    Drink drink;
+    Drink drink2;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        drink = new Drink("Nice Whisky", "10", "whisky", "70,00%", 2014);
+        drink2 = new Drink("Nice Whisky", "10", "whisky", "70,00%", 0);
+    }
+
+    @org.junit.Test
+    public void getName() throws Exception {
+        assertTrue(drink.getName().equals("Nice Whisky"));
+        assertFalse(drink.getName().equals(""));
+    }
+
+    @org.junit.Test
+    public void getPrice() throws Exception {
+        assertTrue(drink.getPrice().equals("10"));
+        assertFalse(drink.getPrice().equals(""));
+    }
+
+    @org.junit.Test
+    public void getType() throws Exception {
+        assertTrue(drink.getType().equals("whisky"));
+        assertFalse(drink.getType().equals(""));
+    }
+
+    @org.junit.Test
+    public void getAlcoholpercentage() throws Exception {
+        assertTrue(drink.getAlcoholpercentage().equals("70,00%"));
+        assertFalse(drink.getAlcoholpercentage().equals(""));
+    }
+
+    @org.junit.Test
+    public void getYear() throws Exception {
+        assertTrue(drink.getYear().equals("2014"));
+        assertFalse(drink.getYear().equals(""));
+        assertTrue(drink2.getYear().equals("N/A"));
+        assertFalse(drink2.getYear().equals(""));
+    }
+
+    @org.junit.Test
+    public void upvote() throws Exception {
+        assertTrue(drink.getUpvotes() == 0);
+        drink.upvote();
+        assertTrue(drink.getUpvotes() == 1);
+    }
+
+    @org.junit.Test
+    public void downvote() throws Exception {
+        assertTrue(drink.getDownVotes() == 0);
+        drink.downvote();
+        assertTrue(drink.getDownVotes() == 1);
+    }
+}
