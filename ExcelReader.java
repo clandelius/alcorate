cd99d6efcb613b6088ebcebc2bea3a5c2ce15f94
landeliu <landeliu@kth.se>
2017-05-06 16:23:40 +0200

    removed excess stuff

--- ExcelReader.java
+++ ExcelReader.java
@@ -1,59 +1,49 @@
 /**
  * Created by Cecilia Landelius on 2017-05-06.
  */
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
  * Reads from the excel document systemet.xls. Uses apache poi, code is inspired from their website.
  */
 public class ExcelReader {
-    public static void main(String[] args) {
+
+    XSSFSheet sheet;
+
+    public ExcelReader (String filename){
         FileInputStream file = null;
         try {
+            file = new FileInputStream(new File(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-            file = new FileInputStream(new File("systemet.xls"));
-
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-            XSSFSheet sheet = workbook.getSheetAt(0);
-
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while (cellIterator.hasNext()) {
-
-                    Cell cell = cellIterator.next();
-
-                    switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_BOOLEAN:
-                            System.out.print(cell.getBooleanCellValue() + "\t\t");
-                            break;
-                        case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "\t\t");
-                            break;
-                        case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + "\t\t");
-                            break;
-                    }
-                }
-                System.out.println("");
-            }
-            file.close();
-            FileOutputStream out =
-                    new FileOutputStream(new File("C:\\test.xls"));
-            workbook.write(out);
-            out.close();
-
+        XSSFWorkbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        sheet = workbook.getSheetAt(0);
+    }
+
+    public ArrayList<Row> getCategories() {
+        ArrayList<Row> rows = new ArrayList<>();
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            String category = row.getCell(11).getStringCellValue();
+            if (category.equals("Whisky") || category.equals("Cognac")) {
+                rows.add(row);
+            }
+        }
+        return rows;
     }
 }
