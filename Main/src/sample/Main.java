d466077b395b8620d0b6df2c34c3ea344de6190c
landeliu <landeliu@kth.se>
2017-05-07 13:51:25 +0200

    adding pictures to main

--- Main/src/sample/Main.java
+++ Main/src/sample/Main.java
@@ -1,17 +1,29 @@
-package sample;
+//package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application{
 
     Stage window;
     Scene startScene, kidScene, mainScene;
+    Image upVoteImage = new Image(getClass().getResourceAsStream("upvote.png"));
+    Image downVoteImage = new Image(getClass().getResourceAsStream("downvote.png"));
 
 
     public static void main(String[] args) {
@@ -22,6 +34,8 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
+        Products products = new Products();
+        ArrayList<Drink> drinks = products.getDrinks();
 
         Label welcome = new Label("Welcome to Alcorate!" );
 
@@ -47,14 +61,102 @@ public class Main extends Application{
         kidLayout.getChildren().add(kidLabel);
         kidScene = new Scene(kidLayout, 300, 300);
 
+
+        //VBox mainLayout = new VBox();
+
+        //ListView<String> list = addList(drinks);
+
+        //mainLayout.getChildren().addAll(welcome, list);
+
         //Alcorate layout
-        VBox mainLayout= new VBox(20);
-        mainLayout.getChildren().add(welcome);
-        mainScene = new Scene(mainLayout, 600, 600);
+        mainScene = new Scene(createContent(drinks), 600, 600);
         startLayout.setAlignment(Pos.CENTER);
 
         window.setScene(startScene);
-        window.setTitle("Project window");
+        window.setTitle("Alcorate");
         window.show();
+    }
+
+    //private ListView<String> addList(ArrayList<Drink> drinks) {
+     //   ListView<String> list = new ListView<>();
+       // ObservableList<String> items = FXCollections.observableArrayList ();
+        //for (Drink drink : drinks) {
+          //  items.add(drink.getDrinkString());
+   //     }
+     //   list.setItems(items);
+       // return list;
+   // }
+
+    public class HBoxCell extends HBox {
+        Label label = new Label();
+        //Button upvote = new Button();
+        //Button downvote = new Button();
+        Label emptyLabel = new Label(" ");
+
+        HBoxCell(String labelText, String buttonText1, String buttonText2, Drink drink) {
+            super();
+
+            label.setText(labelText);
+            label.setMaxWidth(Double.MAX_VALUE);
+            //HBox.setHgrow(label, Priority.ALWAYS);
+
+
+            String downVotes = drink.getDownVotes() + "";
+            String upVotes = drink.getUpvotes() + "";
+
+
+            Button upVote = new Button(upVotes, new ImageView(upVoteImage));
+            Button downVote = new Button(downVotes, new ImageView(downVoteImage));
+
+            upVote.setOnAction(e -> updateView("upvote", window, drink));
+            downVote.setOnAction(e -> updateView("downvote", window, drink));
+
+            //upvote.setText(buttonText1);
+            //downvote.setText(buttonText2);
+
+
+
+            this.getChildren().addAll(label, upVote, emptyLabel, downVote);
+        }
+    }
+
+    public Parent createContent(ArrayList<Drink> drinks) {
+        BorderPane layout = new BorderPane();
+
+        List<HBoxCell> list = new ArrayList<>();
+        for (Drink drink : drinks) {
+            list.add(new HBoxCell(drink.getDrinkString(), "Upvote", "Downvote", drink));
+        }
+
+        ListView<HBoxCell> listView = new ListView<>();
+        ObservableList<HBoxCell> myObservableList = FXCollections.observableList(list);
+        listView.setItems(myObservableList);
+
+        layout.setCenter(listView);
+
+        return layout;
+    }
+
+    private void updateView(String action, Stage primaryStage, Drink drink){
+
+        HBox layout = new HBox();
+
+        if(action.equals("upvote"))
+        {
+            drink.upvote();
+            try {
+                start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            drink.downvote();
+            try {
+                start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
