4c2c5b9443719f0a27f11afd1211eec8ff9d4121
landeliu <landeliu@kth.se>
2017-05-09 18:12:46 +0200

    fixed bug with age comparator

--- AgeComparator.java
+++ AgeComparator.java
@@ -8,16 +8,8 @@ public class AgeComparator implements Comparator<Drink> {
     @Override
     public int compare(Drink d1, Drink d2) {
 
-        if (d1.getYear().equals("N/A")) {
-            d1.setAge();
-        }
-
-        if(d2.getYear().equals("N/A")) {
-            d2.setAge();
-        }
-
-        int d1Year = Integer.parseInt(d1.getYear());
-        int d2Year = Integer.parseInt(d2.getYear());
+        int d1Year = d1.getYear();
+        int d2Year = d2.getYear();
 
         int difference = d1Year - d2Year;
         return difference;
