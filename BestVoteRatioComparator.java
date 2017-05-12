d0392bac1272576e517c1a577b1014991ae67a27
Ramtin <ramtinm@kth.se>
2017-05-12 09:12:46 +0200

    More correct BestVoteRatioComparator

--- BestVoteRatioComparator.java
+++ BestVoteRatioComparator.java
@@ -3,44 +3,37 @@ import java.util.Comparator;
 /**
  * Created by Ramtin on 2017-05-09.
  */
-public class AgeComparator implements Comparator<Drink> {
+public class BestVoteRatioComparator implements Comparator<Drink> {
 
     @Override
     public int compare(Drink d1, Drink d2) {
-
-        int d1Age;
-        int d2Age;
-
-        if (d1.getYear().equals("N/A")) {
-            d1Age = 9999;
-        } else {
-            d1Age = Integer.parseInt(d1.getYear());
-        }
-
-        if (d2.getYear().equals("N/A")) {
-            d2Age = 9999;
-        } else {
-            d2Age = Integer.parseInt(d2.getYear());
-        }
+        int d1UpVotes = d1.getUpvotes()*1000000;
+        int d1DownVotes = d1.getDownVotes()*1000000;
+        int d1TotalVotes = (d1UpVotes + d1DownVotes)/10; //divide total by 10 so that i.e. 10001/10000 doesn't become 1.
 
 
-        int difference = d1Age - d2Age;
-        return difference;
 
-        /*
-        if(difference == 0)
+        int d2UpVotes = d2.getUpvotes()*1000000;
+        int d2DownVotes = d2.getDownVotes()*1000000;
+        int d2TotalVotes = (d2UpVotes + d2DownVotes)/10; //divide total by 10 so that i.e. 10001/10000 doesn't become 1.
+
+        //Avoiding division by zero through the use of if-statements
+        if(d2TotalVotes == 0 && d1TotalVotes == 0) //equal
         {
             return 0;
         }
-        else if(difference > 0){
+        else if(d1TotalVotes != 0 && d2TotalVotes == 0) //d1 > d2
+        {
+            return -1;
+        }
+        else if(d1TotalVotes == 0 && d2TotalVotes != 0) //d2 > d1
+        {
             return 1;
         }
-        return -1;
-    }
-    */
 
-        //could be shortened down to:
-        // return difference;
-        // but I don't know why.
+        int d1Ratio = d1UpVotes/d1TotalVotes;
+        int d2Ratio = d2UpVotes/d2TotalVotes;
+
+        return d2Ratio - d1Ratio;
     }
 }
