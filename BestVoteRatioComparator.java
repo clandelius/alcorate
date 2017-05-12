893a0bfe7a750c69718f5f499cf9777a0c4ea7fc
Ramtin <ramtinm@kth.se>
2017-05-12 09:05:40 +0200

    Correct BestVoteRatioComparator

--- BestVoteRatioComparator.java
+++ BestVoteRatioComparator.java
@@ -3,37 +3,44 @@ import java.util.Comparator;
 /**
  * Created by Ramtin on 2017-05-09.
  */
-public class BestVoteRatioComparator implements Comparator<Drink> {
+public class AgeComparator implements Comparator<Drink> {
 
     @Override
     public int compare(Drink d1, Drink d2) {
-        int d1UpVotes = d1.getUpvotes()*1000000;
-        int d1DownVotes = d1.getDownVotes()*1000000;
-        int d1TotalVotes = d1UpVotes + d1DownVotes;
+
+        int d1Age;
+        int d2Age;
+
+        if (d1.getYear().equals("N/A")) {
+            d1Age = 9999;
+        } else {
+            d1Age = Integer.parseInt(d1.getYear());
+        }
+
+        if (d2.getYear().equals("N/A")) {
+            d2Age = 9999;
+        } else {
+            d2Age = Integer.parseInt(d2.getYear());
+        }
 
 
+        int difference = d1Age - d2Age;
+        return difference;
 
-        int d2UpVotes = d2.getUpvotes()*1000000;
-        int d2DownVotes = d2.getDownVotes()*1000000;
-        int d2TotalVotes = d2UpVotes + d2DownVotes;
-
-        //Avoiding division by zero through the use of if-statements
-        if(d2TotalVotes == 0 && d1TotalVotes == 0) //equal
+        /*
+        if(difference == 0)
         {
             return 0;
         }
-        else if(d1TotalVotes != 0 && d2TotalVotes == 0) //d1 > d2
-        {
-            return -1;
-        }
-        else if(d1TotalVotes == 0 && d2TotalVotes != 0) //d2 > d1
-        {
+        else if(difference > 0){
             return 1;
         }
+        return -1;
+    }
+    */
 
-        int d1Ratio = d1UpVotes/d1TotalVotes;
-        int d2Ratio = d2UpVotes/d2TotalVotes;
-
-        return d2Ratio - d1Ratio;
+        //could be shortened down to:
+        // return difference;
+        // but I don't know why.
     }
 }
