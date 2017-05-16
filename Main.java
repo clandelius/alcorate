bfe8747af6c4192a9aefe85d3320a5dd242bd336
navidha <navidha@kth.se>
2017-05-16 10:30:13 +0200

    Drop Shadow

--- Main.java
+++ Main.java
@@ -6,53 +6,54 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 
 public class Main extends Application{
 
     private Stage window;
-    private Scene startScene, kidScene, mainScene;// , nameScene, priceScene, ageScene, upVoteScene, downVoteScene, bestVoteScene;
+    private Scene startScene, kidScene, mainScene;
     private Image upVoteImage = new Image(getClass().getResourceAsStream("upvote.png"));
     private Image downVoteImage = new Image(getClass().getResourceAsStream("downvote.png"));
     private Products products = new Products();
     private ArrayList<Drink> drinks = products.getDrinks();
-
-
     private ComboBox<String> comboBox;
 
-
-    //First mainScene is sorted by name
-    public Main() {
-        Collections.sort(drinks, new NameComparator()); //sorts the products by name
-    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
 
-        Label welcome = new Label("Welcome to Alcorate!" );
+        DropShadow ds = new DropShadow();
+        ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
+
+        Label welcome = new Label("Welcome to Alcorate" );
         welcome.setTextFill(Color.WHITE);
+        welcome.setEffect(ds);
 
         welcome.setFont(new Font("Arial",50));
-
-        Label kidLabel = new Label("Go get an Ice cream instead");
+        Label kidLabel = new Label("Thank you for using Alcorate");
+        kidLabel.setTextFill(Color.BLACK);
+        kidLabel.setEffect(ds);
         kidLabel.setAlignment(Pos.CENTER);
 
         //Button 1
         Button button1 = new Button("I am under the age of 20");
-        button1.setOnAction(e ->window.setScene(kidScene));
+        button1.setOnAction(e -> window.setScene(kidScene));
 
         //Button 2
         Button button2 = new Button("I am over the age of 20");
@@ -69,15 +70,14 @@ public class Main extends Application{
         root.getChildren().add(startLayout);
         startLayout.setAlignment(Pos.CENTER);
 
-
         //Kid layout
         VBox kidLayout = new VBox(20);
         kidLayout.getChildren().add(kidLabel);
         kidScene = new Scene(kidLayout, 600, 600);
+        kidLayout.setAlignment(Pos.CENTER);
 
         //Alcorate layout
         mainScene = new Scene(createContent(drinks), 600, 600);
-
 
         window.setScene(startScene);
         window.setTitle("Alcorate");
